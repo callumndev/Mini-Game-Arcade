fd91fa58159d86d143d7ed591939fa66fa11c923
callumndev <39914113+callumndev@users.noreply.github.com>
2023-08-06 21:21:53 +0100

    Add preact router

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,21 +1,12 @@
-import { useState } from 'preact/hooks'
+import { Router } from 'preact-router'
+
+import { Home } from '@pages/index'
 
 
 export function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <div class="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
-            <h2 class="text-5xl mb-3 text-black">Counter</h2>
-            <p class="text-black">count is {count}</p>
-
-            <button
-                type="button"
-                className="rounded-md bg-indigo-600  mt-3 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={() => setCount(count => count + 1)}
-            >
-                Add
-            </button>
-        </div>
+        <Router>
+            <Home path="/" />
+        </Router>
     )
 }
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import { h, FunctionalComponent } from 'preact';
+import { useState } from 'preact/hooks'
+
+
+const Home: FunctionalComponent = (): h.JSX.Element => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div class="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+            <h2 class="text-5xl mb-3 text-black">Counter</h2>
+            <p class="text-black">count is {count}</p>
+
+            <button
+                type="button"
+                className="rounded-md bg-indigo-600  mt-3 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={() => setCount(count => count + 1)}
+            >
+                Add
+            </button>
+        </div>
+    )
+}
+
+export default Home;
