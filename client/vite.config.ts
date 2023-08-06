dea1f8608c98c599644c4cf8ea3ba7ac16225f9a
callumndev <39914113+callumndev@users.noreply.github.com>
2023-08-06 20:17:54 +0100

    Add import alias'

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
+
+import path from 'path'
 import preact from '@preact/preset-vite'
 
 
+const clientRoot = path.join(__dirname);
+const clientSrc = path.join(clientRoot, "src");
+
 // https://vitejs.dev/config/
 export default defineConfig({
+    // Array of plugins to use
     plugins: [preact()],
+
+    // Resolve options
+    resolve: {
+        // Import alias
+        alias: {
+            "@": clientSrc,
+            "@assets": path.join(clientSrc, "assets"),
+            "@pages": path.join(clientSrc, "pages"),
+            "@components": path.join(clientSrc, "components"),
+        },
+    },
 })
